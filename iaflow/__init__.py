ca479698edeb11b2ebd0dac82fa3e057aeb94980
enmanuelmag <enmanuelmag@cardor.dev>
2022-07-13 14:56:35 +0200

    Change workflow aith a class

--- iaflow/__init__.py
+++ iaflow/__init__.py
@@ -1,110 +1,17 @@
 import os
-import time
+import gc
 import json
+import time
+import shutil
 import typing as T
 import tensorflow as tf
 import subprocess as sp
 
 from datetime import datetime
 from notifier import Notifier
+from tensorflow.keras.backend import clear_session
 
-
-def find_endwith(path: str, endwith: str):
-  if not os.path.exists(path):
-    os.makedirs(path, exist_ok=True)
-    return None
-
-  for filename in os.listdir(path):
-    if filename.endswith(endwith):
-      return os.path.join(path, filename)
-  return None
-
-def get_params_models(load_model: bool, path_model: str, model_params: T.Dict):
-  if not load_model:
-    return model_params
-  
-  params = None
-  path_params = find_endwith(path_model, '.json')
-  if path_params is not None:
-    with open(path_params, 'r') as file_params:
-      params = json.load(file_params)
-  
-  if params:
-    return params
-
-  return model_params
-
-def create_file(path: str, content: T.Any, mode: str = 'w', is_json: bool = False):
-  if not os.path.exists(path):
-    with open(path, mode) as file:
-      if is_json:
-        json.dump(content, file)
-      else:
-        file.write(content)
-
-def NoImplementedError(message: str):
-  raise NotImplementedError(message)
-
-def build(
-  model_name: str,
-  models_folder: str,
-  run_id: str = None,
-  model_params: T.Dict = {},
-  compile_params: T.Dict = {},
-  load_model_params: T.Union[T.Dict, None] = {},
-  builder_function: T.Callable = lambda **kwargs: NoImplementedError('Builder function not implemented')
-) -> T.Tuple[tf.keras.Model, str, str, str]:
-
-  load_model = run_id is not None
-
-  model_params = get_params_models(load_model, models_folder, model_params)
-  model_params_str = '_'.join(map(str, model_params.values()))
-  model_ident = '_'.join([ model_name, model_params_str ])
-
-  if run_id is None or len(run_id) == 0:
-    run_id = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
-  path_model = os.path.join(models_folder, model_name, run_id)
-  
-  if not os.path.exists(path_model):
-    os.makedirs(path_model)
-  
-  if load_model:
-    check_path = find_endwith(path_model, '.h5')
-    if check_path is None:
-      raise Exception(f'Model not found at {path_model}')
-  else:
-    check_path = f'{path_model}/{model_ident}_checkpoint.h5'
-
-  os.makedirs(path_model, exist_ok=True)
-
-  path_params = f'{path_model}/{model_name}_params.json'
-
-  create_file(path_params, model_params, is_json=True)
-
-  if os.path.isfile(check_path) and load_model:
-    found_model = True
-    print(f'Model found, building saved model at {check_path}')
-  else:
-    found_model = False
-    print('Creating new model')
-
-  try:
-    gpu_info = tf.config.experimental.list_physical_devices('GPU')
-  except:
-    gpu_info = ''
-
-  info_env = gpu_info[0].name if len(gpu_info) > 0 else 'Not connected to a GPU'
-  print(f'GPU info: {info_env}\n\n')
-
-  if found_model:
-    load_model_params['filepath'] = check_path
-    model = tf.keras.models.load_model(**load_model_params)
-  else:
-    model = builder_function(**model_params)
-    model.compile(**compile_params)
-
-  return model, run_id, model_ident, check_path, path_model
-
+SLIP_KEYS = [ 'run_id', 'model_name' ]
 
 class NotifierCallback(tf.keras.callbacks.Callback):
 
@@ -141,7 +48,6 @@ class NotifierCallback(tf.keras.callbacks.Callback):
     except Exception as e:
       print('There was an error sending the notification:', e)
 
-
 class ParamsNotifier(T.TypedDict):
   title: T.Optional[str]
   email: T.Optional[str]
@@ -149,44 +55,254 @@ class ParamsNotifier(T.TypedDict):
   api_token: T.Optional[str]
   webhook_url: T.Optional[str]
 
-def train(
-  model: tf.keras.Model,
-  check_path: str,
-  path_model: str,
-  train_ds: T.Union[tf.data.Dataset, T.List[T.Any], T.Any],
-  val_ds: T.Union[tf.data.Dataset, T.List[T.Any], T.Any],
-  batch: int = 32,
-  epochs: int = 100,
-  initial_epoch: int = 0,
-  checkpoint_params: T.Dict = {},
-  tensorboard_params: T.Dict = {},
-  params_notifier: ParamsNotifier = None,
-  callbacks: T.Union[T.List[T.Any], T.Any] = [],
-):
-  assert isinstance(train_ds, tf.data.Dataset) and isinstance(val_ds, tf.data.Dataset)
+def NoImplementedError(message: str):
+  raise NotImplementedError(message)
 
-  if params_notifier:
-    callbacks.append(NotifierCallback(**params_notifier))
+
+class IAFlow(object):
+
+  def __init__(self,
+    models_folder: str,
+    builder_function: T.Callable = lambda **kwargs: NoImplementedError('Builder function not implemented'),
+    batch: int = 32,
+    epochs: int = 100,
+    checkpoint_params: T.Dict = {},
+    tensorboard_params: T.Dict = {},
+    params_notifier: ParamsNotifier = None,
+    callbacks: T.Union[T.List[T.Any], T.Any] = [],
+    val_ds: T.Union[tf.data.Dataset, T.List[T.Any], T.Any] = None,
+    train_ds: T.Union[tf.data.Dataset, T.List[T.Any], T.Any] = None,
+  ):
+    self.models = {}
+    self.callbacks = callbacks
+    self.models_folder = models_folder
+    self.params_notifier = params_notifier
+    self.builder_function = builder_function
+    self.checkpoint_params = checkpoint_params
+    self.tensorboard_params = tensorboard_params
+    self.batch = batch
+    self.epochs = epochs
+    self.val_ds = val_ds
+    self.train_ds = train_ds
+
+    self.notifier = NotifierCallback(**params_notifier) if params_notifier else None
+
+  def __delete_by_path(self, path: str, is_dir: bool = False):
+    if is_dir:
+      shutil.rmtree(path, ignore_errors=True)
+    else:
+      try:
+        os.remove(path, ignore_errors=True)
+      except:
+        pass
+
+  def __find_endwith(self, path: str, endwith: str):
+    if not os.path.exists(path):
+      os.makedirs(path, exist_ok=True)
+      return None
+
+    for filename in os.listdir(path):
+      if filename.endswith(endwith):
+        return os.path.join(path, filename)
+    return None
+
+  def __get_params_models(self, load_model: bool, path_model: str, model_params: T.Dict):
+    if not load_model:
+      return model_params
+    
+    params = None
+    path_params = __find_endwith(path_model, '.json')
+    if path_params is not None:
+      with open(path_params, 'r') as file_params:
+        params = json.load(file_params)
+    
+    if params:
+      return params
+
+    return model_params
+
+  def __create_file(self, path: str, content: T.Any, mode: str = 'w', is_json: bool = False):
+    if not os.path.exists(path):
+      with open(path, mode) as file:
+        if is_json:
+          json.dump(content, file)
+        else:
+          file.write(content)
+
+  def set_builder_function(self, builder_function: T.Callable):
+    self.builder_function = builder_function
+
+  def get_model(self, model_name: str, run_id: str, force_creation: bool = True):
+    
+    model = self.models.get(model_name)
+    if model is None:
+      print(f'Model {model_name} not found')
+      return None
+    
+    run_id_data = model.get(run_id)
+    if run_id_data is None:
+      print(f'Run {run_id} not found')
+      return None
+
+    check_path = run_id_data.get('check_path')
+    if os.path.exists(check_path) and not force_creation:
+      print(f'Loading model from {check_path}')
+      model = tf.keras.models.load_model(**run_id_data.get('load_model_params'))
+    else:
+      print(f'Force creation is {force_creation}. Deleting old logs and model')
+      self.__delete_by_path(run_id_data.get('log_dir'), is_dir=True)
+      self.__delete_by_path(run_id_data.get('check_path'), is_dir=False)
+      
+      print('Creating model')
+      model = self.builder_function(**run_id_data.get('model_params'))
+      model.compile(**run_id_data.get('compile_params'))
+
+    return model, run_id_data
+
+  def delete_model(self, model_data: T.Dict, delete_folder: bool = False):
+    model_name = model_data.get('model_name')
+    run_id = model_data.get('run_id')
   
-  callbacks.extend([
-    tf.keras.callbacks.ModelCheckpoint(check_path, **checkpoint_params),
-    tf.keras.callbacks.TensorBoard(log_dir=f'{path_model}/logs', **tensorboard_params)
-  ])
+    model = self.models.get(model_name)
+    if model is None:
+      print(f'Model {model_name} not found')
+      return None
+    
+    run_id_data = model.get(run_id)
+    if run_id_data is None:
+      print(f'Run {run_id} not found')
+      return None
 
-  use_tf_dataset = False
-  if isinstance(train_ds, tf.data.Dataset) and isinstance(val_ds, tf.data.Dataset):
-    use_tf_dataset = True
-    train_ds = train_ds.batch(batch)
-    val_ds = val_ds.batch(batch)
+    if delete_folder:
+      self.__delete_by_path(run_id_data.get('path_model'), is_dir=True)
+    
+    del model[run_id]
+    if len(model) == 0:
+      del self.models[model_name]
+    
+    print(f'Model {model_name}/{run_id} deleted')
 
-  start_time = time.time()
-  model.fit(
-    train_ds,
-    epochs=epochs,
-    callbacks=callbacks,
-    validation_data=val_ds,
-    initial_epoch=initial_epoch,
-    batch_size=batch if not use_tf_dataset else None,
-  )
+  def show_models(self):
+    print('Models:')
+    for model_name, model in self.models.items():
+      print(f'  {model_name}')
+      for run_id, run_id_data in model.items():
+        print(f'    {run_id}')
+        for key, value in run_id_data.items():
+          if key not in SLIP_KEYS:
+            print(f'      {key}: {value}')
 
-  print(f'Training time: {time.time() - start_time}')
+  def add_model(
+    self,
+    model_name: str,
+    model_params: T.Dict = {},
+    compile_params: T.Dict = {},
+    load_model_params: T.Union[T.Dict, None] = {}
+  ) -> T.Tuple[tf.keras.Model, str, str, str]:
+
+    models_folder = self.models_folder
+    model_params = self.__get_params_models(False, models_folder, model_params)
+    model_params_str = '_'.join(map(str, model_params.values()))
+    model_ident = '_'.join([ model_name, model_params_str ])
+
+    
+    run_id = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
+    path_model = os.path.join(models_folder, model_name, run_id)
+
+    if not os.path.exists(path_model):
+      os.makedirs(path_model)
+    
+    check_path = f'{path_model}/{model_ident}_checkpoint.h5'
+
+    os.makedirs(path_model, exist_ok=True)
+    path_params = f'{path_model}/{model_name}_params.json'
+    self.__create_file(path_params, model_params, is_json=True)
+
+    try:
+      gpu_info = tf.config.experimental.list_physical_devices('GPU')
+    except:
+      gpu_info = ''
+
+    info_env = gpu_info[0].name if len(gpu_info) > 0 else 'Not connected to a GPU'
+    print(f'GPU info: {info_env}\n\n')
+
+    model_data = self.models.get(model_name, {})
+    
+    load_model_params['filepath'] = check_path
+    model_data[run_id] = {
+      'run_id': run_id,
+      'model_name': model_name,
+      'model_ident': model_ident,
+      'model_params': model_params,
+      'compile_params': compile_params,
+      'load_model_params': load_model_params,
+      'check_path': check_path,
+      'path_model': path_model,
+      'log_dir': f'{path_model}/logs',
+    }
+
+    self.models[model_name] = model_data
+    return model_data[run_id]
+
+  def train(
+    self,
+    model_data: T.Dict,
+    train_ds: T.Union[tf.data.Dataset, T.List[T.Any], T.Any] = None,
+    val_ds: T.Union[tf.data.Dataset, T.List[T.Any], T.Any] = None,
+    force_creation: bool = False,
+    batch: int = 32,
+    epochs: int = 100,
+    initial_epoch: int = 0
+  ):
+    batch = batch or self.batch
+    epochs = epochs or self.epochs
+    val_ds = val_ds or self.val_ds
+    train_ds = train_ds or self.train_ds
+
+    model_name = model_data.get('model_name')
+    run_id = model_data.get('run_id')
+
+    self.clear_session()
+    model, run_data = self.get_model(model_name, run_id, force_creation)
+
+    if self.notifier:
+      self.callbacks.append(self.notifier)
+
+    self.checkpoint_params['filepath'] = run_data.get('check_path')
+    self.callbacks.append(tf.keras.callbacks.ModelCheckpoint(**self.checkpoint_params))
+
+    self.tensorboard_params['log_dir'] = run_data.get('log_dir')
+    self.callbacks.append(tf.keras.callbacks.TensorBoard(**self.tensorboard_params))
+
+    use_tf_dataset = False
+    if isinstance(train_ds, tf.data.Dataset) and isinstance(val_ds, tf.data.Dataset):
+      use_tf_dataset = True
+      train_ds = train_ds.batch(batch)
+      val_ds = val_ds.batch(batch)
+
+    start_time = time.time()
+    model.fit(
+      train_ds,
+      epochs=epochs,
+      callbacks=self.callbacks,
+      validation_data=val_ds,
+      initial_epoch=initial_epoch,
+      batch_size=batch if not use_tf_dataset else None,
+    )
+    print(f'Training time: {time.time() - start_time}')
+
+    self.clear_session()
+    del model
+
+  def save(self, path: str):
+    data = self.models
+    path = path or self.models_folder
+
+    with open(f'{path}/models.json', 'w') as file:
+      json.dump(data, file)
+    print(f'Saved data to {path}/models.json')
+
+  def clear_session(self):
+    clear_session()
+    gc.collect()
+
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
+import json
 import tensorflow as tf
-import iaflow as iaf
+from iaflow import IAFlow
 
 
 def custom_builder(input_shape):
@@ -12,29 +13,11 @@ def custom_builder(input_shape):
   ])
   return model
 
-
-model, run_id, model_ident, check_path, path_model = iaf.build(
-  model_name='model_dense', # Name of the model
-  models_folder='./models', # Path to folder where models will be saved
-  run_id=None, # Optional run id to load model
-  builder_function=custom_builder, # Function to build your or yours models
-  model_params={ 'input_shape': (2, 1) }, # Parameters for builder function
-  compile_params={ # Parameters for model compilation, see documentation tf.keras.models.compile
-    'optimizer': 'adam', 'loss': 'mse',
-    'metrics': ['accuracy']
-  },
-  load_model_params={} # Parameters for model loading, see documentation of tf.keras.models.load_model
-)
-
-print(
-f"""
-Model: {model} (the tf keras model)
-Run id: {run_id} (this is a unique identifier for this run)
-Model path: {path_model} (this is the path to the model folder)
-Checkpoint path: {check_path} (this is the path to the model checkpoint)
-Model ident: {model_ident} (this is a unique identifier for this model, include model name and parameters)
-"""
-)
+params_notifier = { # Parameters for notifier, see documentation https://pypi.org/project/notify-function/#description
+  'title': 'Training update',
+  'webhook_url': os.environ.get('WEBHOOK_URL'),
+  'frequency_epoch': 20 # This will send a notification every 20 epochs, by default it is every epoch
+}
 
 all_data = tf.data.Dataset.from_tensor_slices((
   tf.random.uniform([1000, 2]),
@@ -43,24 +26,34 @@ all_data = tf.data.Dataset.from_tensor_slices((
 train_ds = all_data.take(5)
 val_ds = all_data.skip(5)
 
-
-
-iaf.train(
-  model=model, # The tf keras model
-  check_path=check_path, # Path to the model checkpoint
-  path_model=path_model, # Path to the model folder
-  train_ds=train_ds, # Training dataset, could be a tf.data.Dataset, a list of tensors or a tensor
-  val_ds=val_ds, # Validation dataset, could be a tf.data.Dataset, a list of tensors or a tensor
-  batch=32, # Batch size
-  epochs=100, # Number of epochs
-  initial_epoch=0, # Initial epoch
-  checkpoint_params={ # Parameters for model checkpoint, see documentation tf.keras.callbacks.ModelCheckpoint
-    'monitor': 'accuracy', 'verbose': 1,
-    'save_best_only': False, 'save_freq':'epoch'
-  },
-  params_notifier={ # Parameters for notifier, see documentation https://pypi.org/project/notify-function/#description
-    'title': 'Training update',
-    'webhook_url': os.environ.get('WEBHOOK_URL'),
-    'frequency_epoch': 20 # This will send a notification every 20 epochs, by default it is every epoch
-  }
+# Creation of the IAFlow instance
+ia_maker = IAFlow(
+  val_ds=val_ds,
+  train_ds=train_ds,
+  models_folder='./models',
+  params_notifier=params_notifier,
+  builder_function=custom_builder
 )
+
+model_1_data = ia_maker.add_model(
+  model_name='model_1',
+  model_params={ 'input_shape': (2, 1) }, # Parameters for builder function
+  load_model_params={}, # Parameters for model loading, see documentation of tf.keras.models.load_model
+  compile_params={ # Parameters for model compilation, see documentation tf.keras.models.compile
+    'optimizer': 'adam', 'loss': 'mse',
+    'metrics': ['accuracy']
+  },
+)
+print(f'Data of model_1: {json.dumps(model_1_data, indent=4)}')
+
+print('Show all models:')
+ia_maker.show_models()
+
+ia_maker.train(model_1_data, batch=32, epochs=5)
+
+ia_maker.clear_session()
+
+print('Lading model trained')
+ia_maker.train(model_1_data, batch=32, epochs=5)
+
+ia_maker.delete_model(model_1_data, delete_folder=True)
